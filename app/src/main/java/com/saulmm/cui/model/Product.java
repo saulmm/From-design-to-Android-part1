d145ff59a60697b14318e6fe85308ab724f4e379
Saul Molinero <saulmm2@gmail.com>
2017-01-28 22:09:43 +0100

    Added product images and product image setting

--- app/src/main/java/com/saulmm/cui/model/Product.java
+++ app/src/main/java/com/saulmm/cui/model/Product.java
@@ -4,21 +4,29 @@ package com.saulmm.cui.model;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.saulmm.cui.model.Product.ID.*;
+
 public class Product {
+    public enum ID {
+        ALL_STAR, SANDAL, WOMEN_SHOES, SPORT_SHOES;
+    }
+
     public final String name;
+    public final ID productId;
     public final String price;
 
-    public Product(String name, String price) {
+    private Product(String name, String price, ID productId) {
         this.name = name;
         this.price = price;
+        this.productId = productId;
     }
 
     public static List<Product> createFakeProducts() {
         return Arrays.asList(
-            new Product("Shooting Stars", "$45"),
-            new Product("Pictures in Sky", "$575"),
-            new Product("The basics of buying a telescope", "$892"),
-            new Product("The skyrider", "$23")
+            new Product("Shooting Stars", "$45", ALL_STAR),
+            new Product("Pictures in Sky", "$575", SANDAL),
+            new Product("The basics of buying a telescope", "$892", WOMEN_SHOES),
+            new Product("The skyrider", "$23", SPORT_SHOES)
         );
     }
 }
--- app/src/main/java/com/saulmm/cui/recycler/ProductViewHolder.java
+++ app/src/main/java/com/saulmm/cui/recycler/ProductViewHolder.java
@@ -1,19 +1,55 @@
 package com.saulmm.cui.recycler;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 
+import com.saulmm.cui.R;
 import com.saulmm.cui.databinding.ItemProductBinding;
 import com.saulmm.cui.model.Product;
 
 public class ProductViewHolder extends RecyclerView.ViewHolder {
     private final ItemProductBinding binding;
+    private final Context context;
 
     ProductViewHolder(ItemProductBinding binding) {
         super(binding.getRoot());
         this.binding = binding;
+        this.context = itemView.getContext();
     }
 
     void bind(Product product) {
         binding.setProduct(product);
+        setProductImage(product.productId);
+    }
+
+    private void setProductImage(Product.ID productId) {
+        Drawable productDrawable = null;
+
+        switch (productId) {
+            case ALL_STAR:
+                productDrawable = ContextCompat.getDrawable(
+                    context, R.drawable.product_allstar);
+                break;
+
+            case SANDAL:
+                productDrawable = ContextCompat.getDrawable(
+                    context, R.drawable.product_sandal);
+                break;
+
+            case WOMEN_SHOES:
+                productDrawable = ContextCompat.getDrawable(
+                    context, R.drawable.product_women_shoes);
+                break;
+
+            case SPORT_SHOES:
+                productDrawable = ContextCompat.getDrawable(
+                    context, R.drawable.product_sport_shoes);
+                break;
+        }
+
+        binding.image.setImageDrawable(productDrawable);
+
     }
 }
