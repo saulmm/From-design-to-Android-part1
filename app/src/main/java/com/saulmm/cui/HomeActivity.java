ab3019e0aefb05e6eaf61ceaf18a12bdeabdedb9
Saul Molinero <saulmm2@gmail.com>
2017-01-28 19:26:35 +0100

    Deleted template generated code and modified HomeActivity

--- app/src/main/java/com/saulmm/cui/HomeActivity.java
+++ app/src/main/java/com/saulmm/cui/HomeActivity.java
@@ -4,25 +4,27 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private final String [] PRODUCTS = new String[] {
+        "Shooting Stars", "Pictures in Sky", "The basics of buying a telescope",
+        "The skyrider"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-            }
-        });
+    private static class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 }
