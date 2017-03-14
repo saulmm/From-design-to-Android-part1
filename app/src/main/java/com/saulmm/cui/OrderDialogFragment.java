bb95260498fd22e225c6f7edbd7edb41e572ac8a
Saul Molinero <saulmm2@gmail.com>
2017-03-14 12:46:18 +0100

    Added license

--- app/src/main/java/com/saulmm/cui/OrderDialogFragment.java
+++ app/src/main/java/com/saulmm/cui/OrderDialogFragment.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.saulmm.cui;
 
 import android.databinding.BindingAdapter;
@@ -25,7 +40,6 @@ import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AccelerateInterpolator;
 import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -99,7 +113,7 @@ public class OrderDialogFragment extends BottomSheetDialogFragment {
         orderSelection = new OrderSelection();
 
         selectedViewTransition = TransitionInflater.from(getContext())
-            .inflateTransition(R.transition.transition_fake_view);
+            .inflateTransition(R.transition.transition_selected_view);
 
         binding.setProduct(getProduct());
         binding.imgProduct.setImageDrawable(createProductImageDrawable(getProduct()));
