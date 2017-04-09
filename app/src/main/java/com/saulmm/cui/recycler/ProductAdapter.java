5438a17900b14b8ac4e4af8cc91eab98e5b19adf
Saul Molinero <saulmm2@gmail.com>
2017-04-09 02:13:25 +0200

    Moved ProductViewHolder as a inner class of ProductAdapter

--- app/src/main/java/com/saulmm/cui/recycler/ProductAdapter.java
+++ app/src/main/java/com/saulmm/cui/recycler/ProductAdapter.java
@@ -15,16 +15,21 @@
  */
 package com.saulmm.cui.recycler;
 
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import com.saulmm.cui.R;
 import com.saulmm.cui.databinding.ItemProductBinding;
 import com.saulmm.cui.model.Product;
 
 import java.util.List;
 
-public class ProductAdapter extends RecyclerView.Adapter<ProductViewHolder> {
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
     private final List<Product> products;
 
     public ProductAdapter(List<Product> products) {
@@ -47,5 +52,42 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductViewHolder> {
     @Override
     public int getItemCount() {
         return products.size();
+    }
+
+
+    static class ProductViewHolder extends RecyclerView.ViewHolder {
+        private final ItemProductBinding binding;
+
+        ProductViewHolder(ItemProductBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        void bind(Product product) {
+            binding.setProduct(product);
+            binding.imgProduct.setBackground(createProductBackground(product));
+            binding.imgProduct.setImageResource(product.image);
+        }
+
+        @NonNull
+        private GradientDrawable createProductBackground(Product product) {
+            final GradientDrawable gradientDrawable = (GradientDrawable) ContextCompat.getDrawable(
+                itemView.getContext(), R.drawable.bg_product);
+
+            gradientDrawable.setColor(ContextCompat.getColor(
+                itemView.getContext(), product.color));
+
+            gradientDrawable.setSize(itemView.getWidth(), getDrawableHeight());
+            gradientDrawable.mutate();
+            return gradientDrawable;
+        }
+
+        private int getDrawableHeight() {
+            final Context context = itemView.getContext();
+
+            return getAdapterPosition() % 2 == 0
+                ? context.getResources().getDimensionPixelOffset(R.dimen.product_regular_height)
+                : context.getResources().getDimensionPixelOffset(R.dimen.product_large_height);
+        }
     }
 }
